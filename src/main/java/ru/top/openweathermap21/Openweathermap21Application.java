b7bcc397fc856da42edbe09e88eb2e7ced5c2f23
MaksimNorvatkin <norvatkin_maksim@mail.ru>
2026-01-11 20:30:33 +0300

    Старт приложения о погоде

---
+++ src/main/java/ru/top/openweathermap21/Openweathermap21Application.java
@@ -0,0 +1,13 @@
+package ru.top.openweathermap21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Openweathermap21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Openweathermap21Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/top/openweathermap21/Openweathermap21ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.top.openweathermap21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Openweathermap21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
